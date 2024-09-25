997829ec9c98d42d190d63c239663150d0ba6cb0
NCD <104053975+Caodangdev@users.noreply.github.com>
2024-09-25 14:29:25 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "ĐÉOOOOOOOOOOOO",
   titleModar: "Cảm ơn embe nhaaa.",
   descriptionModar:
-    "Cảm ơn bé đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và yêu thương NHI. </br> Anh biết đôi khi tính còn hơi trẻ con đã làm cho bé buồn khóc vì anh thì anh cũng xin lỗi bé nhiều. Mong rằng khoảng thời gian sắp tới 2 đứa sẽ dịu dàng , yêu thương nhau nhiều như lúc ban đầu, anh hi vọng là vậy <3  </br> CHO ANH CHỊCH BÉ CÁI NHA =))</br> Love you <3",
+    "Cảm ơn bé đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và yêu thương NHI. </br> Anh biết đôi khi tính còn hơi trẻ con đã làm cho bé buồn khóc vì anh thì anh cũng xin lỗi bé nhiều. Mong rằng khoảng thời gian sắp tới 2 đứa sẽ dịu dàng , yêu thương nhau nhiều như lúc ban đầu, anh hi vọng là vậy <3  </br> love you </br> CHO ANH CHỊCH EM BÉ CÁI NHA =))",
 };
